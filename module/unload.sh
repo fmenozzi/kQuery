6947e1a1cc9b7126b2bc7232e50234d5f0035751
Federico Menozzi <federicogmenozzi@gmail.com>
2015-02-26 10:27:35 -0500

    Suppress output when files don't exist

--- module/unload.sh
+++ module/unload.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-sudo rmmod kquery_mod 
-rm kquery_mod.k*
-rm kquery_mod.m*
-rm kquery_mod.o
-rm Module.symvers
-rm modules.order
+# Suppress output if files don't exist
+sudo rmmod kquery_mod 2> /dev/null
+rm kquery_mod.k*      2> /dev/null
+rm kquery_mod.m*      2> /dev/null
+rm kquery_mod.o       2> /dev/null
+rm Module.symvers     2> /dev/null
+rm modules.order      2> /dev/null
 
